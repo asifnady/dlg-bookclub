ba68f60213efca07b54803b38abc3c16bcd89104
Asif Nadeem <a.nadeem89@gmail.com>
2026-07-03 01:29:04 +0500

    Slow down Star Wars crawl: 35s → 60s

--- app/page.tsx
+++ app/page.tsx
@@ -83,7 +83,7 @@ export default function HomePage() {
           transform-origin: 50% 100%;
           transform: perspective(400px) rotateX(18deg);
           height: 200vh;
-          animation: scrollUp 35s linear infinite;
+          animation: scrollUp 60s linear infinite;
         }
 
         .crawl-content {
